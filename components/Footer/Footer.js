6242bacf56cf37fd49e859e0de13bb642f538533
richhdev <79666708+richhdev@users.noreply.github.com>
2022-07-10 17:21:08 +1000

    add Footer component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+const Footer = () => {
+  return (
+    <>
+      <img src="/img/hr.png" alt="hr" width="1294" height="5" className="hr" />
+      <div id="footer" className="wrapper">
+        <p className="medium">
+          <strong>
+            We accept Cash, Cheque, Visa, Mastercard or Electronic Transfer{" "}
+          </strong>
+        </p>
+        <p>
+          &copy; Copyright 2022 - All Districts Washing Machine Service - Call
+          Mario 0418 540 353 - Melbourne
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
